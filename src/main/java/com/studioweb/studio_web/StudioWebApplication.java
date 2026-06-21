49211275b02936214cefbb83d87c7f898726e646
Antonio Eduardo Moreira Oliveira <eduardo_m_cdc@hotmail.com>
2026-06-20 23:07:14 -0300

    commit inicial

---
+++ src/main/java/com/studioweb/studio_web/StudioWebApplication.java
@@ -0,0 +1,13 @@
+package com.studioweb.studio_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studioweb/studio_web/StudioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studioweb.studio_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
