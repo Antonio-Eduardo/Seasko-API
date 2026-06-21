a1f37b0dd50893df6e096e35a220f7a89b88ce70
Antonio Eduardo Moreira Oliveira <eduardo_m_cdc@hotmail.com>
2026-06-20 23:37:20 -0300

    feat: implementando AppointmentRepository

---
+++ src/main/java/com/studioweb/studio_web/appointment/AppointmentRepository.java
@@ -0,0 +1,6 @@
+package com.studioweb.studio_web.appointment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
+}
