98dc926d267970ca01f35150a4b8703468b56470
Antonio Eduardo Moreira Oliveira <eduardo_m_cdc@hotmail.com>
2026-06-23 02:02:10 -0300

    feat: testando commit

--- src/main/java/com/agilo/user/dto/UsuarioDtoRequest.java
+++ src/main/java/com/agilo/user/dto/UsuarioDtoRequest.java
@@ -4,6 +4,7 @@ import com.agilo.user.UsuarioRole;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+
 public record UsuarioDtoRequest(
         @NotBlank String nome,
         @NotBlank String usuario,
